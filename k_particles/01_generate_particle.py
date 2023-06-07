ae7669c6f96482f11f07c0a0763e0a10b7741a75
Omer Faruk Gulban <farukgulban@gmail.com>
2023-06-07 16:37:40 +0200

    wip

---
+++ k_particles/01_generate_particle.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+
+# Dimensions of the 2D wave
+width = 512
+height = 512
+
+# Generate random parameters for the sine wave
+frequency_x = np.random.uniform(low=0.01, high=1)
+frequency_y = np.random.uniform(low=0.01, high=1)
+phase_x = np.random.uniform(low=0, high=2*np.pi)
+phase_y = np.random.uniform(low=0, high=2*np.pi)
+
+# Create a grid of x and y values
+x = np.linspace(0, width-1, width)
+y = np.linspace(0, height-1, height)
+grid_x, grid_y = np.meshgrid(x, y)
+
+# Generate the 2D sine wave
+wave = np.sin(frequency_x * grid_x + phase_x) + np.sin(frequency_y * grid_y + phase_y)
+wave /= 2
+print(np.percentile(wave, [0, 100]))
+
+# Save the original wave as a PNG image using OpenCV
+wave_scaled = ((wave + 1) * 127.5)
+print(np.percentile(wave_scaled, [0, 100]))
+
+# Wrap
+cv2.imwrite('wave.png', wave_scaled.astype(np.uint8))
+
+# Perform the FFT to obtain the frequency domain representation
+fft_result = np.fft.fftshift(np.fft.fft2(wave))
+
+# Calculate the magnitude spectrum
+magnitude_spectrum = np.abs(fft_result)
+
+# Scale the magnitude spectrum to 8-bit range (0-255)
+scaled_spectrum = ((magnitude_spectrum / magnitude_spectrum.max()) * 255).astype(np.uint8)
+
+# Save the magnitude spectrum as a PNG image using OpenCV
+cv2.imwrite('fft_image.png', scaled_spectrum)
+
+# Print a message indicating the images have been saved
+print("Wave image saved as wave.png")
+print("FFT image saved as fft_image.png")
