f8ffe05d8ab0bfe0994992d40c38195dcba8b6cd
Omer Faruk Gulban <farukgulban@gmail.com>
2023-06-08 18:09:29 +0200

    wip

--- k_particles/03_generate_k_particles.py
+++ k_particles/03_generate_k_particles.py
@@ -2,35 +2,35 @@ import os
 import numpy as np
 import cv2
 
-OUTDIR = "/Users/faruk/Documents/temp-k_particles"
+OUTDIR = "/home/faruk/Documents/temp-k_particles/03_trajectory_line"
 
 DIMS = [512, 512]
 
 PART = [250, 256]
 
 for i in range(0, 256, 4):
-	x, y = PART[0], PART[1] - i
+    x, y = PART[0], PART[1] - i
 
-	# Lattice
-	kspace = np.zeros(DIMS, dtype=complex)
+    # Lattice
+    kspace = np.zeros(DIMS, dtype=complex)
 
-	# Insert particle
-	kspace[x, y] = 1. + 0j
-	kspace[512-x, 512-y] = 1. + 0j
+    # Insert particle
+    kspace[x, y] = 1. + 0j
+    kspace[512-x, 512-y] = 1. + 0j
 
-	# Save
-	img = np.real(kspace) * 255
-	outname = os.path.join(OUTDIR, "test1-{}.png".format(str(i).zfill(4)))
-	print(outname)
-	cv2.imwrite(outname, img.astype(np.uint8))
+    # Save
+    img = np.real(kspace) * 255
+    outname = os.path.join(OUTDIR, "test1-{}.png".format(str(i).zfill(4)))
+    print(outname)
+    cv2.imwrite(outname, img.astype(np.uint8))
 
-	# Reconstruct image
-	ispace = np.fft.ifft2(np.fft.ifftshift(kspace))
+    # Reconstruct image
+    ispace = np.fft.ifft2(np.fft.ifftshift(kspace))
 
-	# Save
-	img = np.abs(ispace)
-	img = img / np.max(img) * 255
-	outname = os.path.join(OUTDIR, "test2-{}.png".format(str(i).zfill(4)))
-	cv2.imwrite(outname, img.astype(np.uint8))
+    # Save
+    img = np.abs(ispace)
+    img = img / np.max(img) * 255
+    outname = os.path.join(OUTDIR, "test2-{}.png".format(str(i).zfill(4)))
+    cv2.imwrite(outname, img.astype(np.uint8))
 
 print("Finished.")
